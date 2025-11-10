d167b3574b22657ffb93792cdde15282c3480393
Chaturya <89186153+Csral@users.noreply.github.com>
2025-11-10 08:15:02 +0530

    Implement running sum of 1D array

---
+++ C++/Running Sum of 1D Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        
+        vector<int> res;
+        int s = 0;
+
+        for (int num : nums) {
+            s += num;
+            res.push_back(s);
+        }
+
+        return res;
+
+    }
+};
